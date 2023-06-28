19087c30fe36aa8c91d5018cd1fec0178b4f1663
Surinder kumar <kumarsonuy6@gmail.com>
2023-06-28 17:37:41 +0530

    did some changes in 2 components

--- src/Data.js
+++ src/Data.js
@@ -4,19 +4,19 @@ const parts = [{
 	name: "head",
 
 }, {
-	id: "1254125124",
+	id: "1254125125",
 	image: "https://tse4.mm.bing.net/th?id=OIP.Zu68p2Dw7YMoS0ih4g0ZkAAAAA&pid=Api&P=0&h=180",
 	name: "chest",
 }, {
-	id: "1254125124",
+	id: "1254125126",
 	image: "https://tse3.mm.bing.net/th?id=OIP.oOUvmuwIqvyWBg2ghwE9DgHaDi&pid=Api&P=0&h=180",
 	name: "rightArm",
 }, {
-	id: "1254125124",
+	id: "1254125127",
 	image: "https://tse1.mm.bing.net/th?id=OIP.jIOKA7rWuzhsZnut2hRZrQHaDy&pid=Api&P=0&h=180",
 	name: "leftArm",
 }, {
-	id: "1254125124",
+	id: "1254125128",
 	image: "https://img.freepik.com/premium-photo/image-cyber-prosthetic-legs-orange-background-created-using-generative-ai-technology_772924-1729.jpg?size=626&ext=jpg&uid=R88439026&semt=ais",
 	name: "legs",
 }];
